293583dedbb7edb68f22c21ea003605e9e8ed3bd
Roks <153099220+Rokssssss@users.noreply.github.com>
2024-01-09 00:18:06 +0500

    Задание 2.cpp

---
+++ Задание 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ cout << "Every"<< "\n";
+ cout << "\tHunter" << "\n";
+ cout << "\t\tWants" << "\n";
+ cout << "\t\t\tTo know" << "\n";
+ cout << "\t\t\t\t Where" << "\n";
+ cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+ cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+
+
+    return 0;
+}
